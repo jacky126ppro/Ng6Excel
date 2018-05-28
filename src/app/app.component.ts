c69d0b645097861f8fb99e5d4829493b110dd21d
yuanchen <jacky126pro@msn.com>
2018-05-28 23:50:43 +0800

    excel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  dropzoneStyle: any = null;
+  reader: FileReader = new FileReader();
+  files: FileList = null;
+  workBook: XLSX.WorkBook = null;
+  workSheetNames: string[] = [];
+  workSheets: { [sheet: string]: XLSX.WorkSheet } = null;
+  currentWorkSheet: XLSX.WorkSheet = null;
+  currentData: any[] = [];
+  constructor() {
+    this.reader.onload = this.setReaderOnloadHandler();
+  }
+
+  readFile($event) {
+    let files = $event.target.files;
+    this.files = files;
+    console.log(files);
+    this.dropzoneStyle = { borderColor: 'green' };
+
+    // 取第一個檔
+    let file = files[0];
+
+    this.reader.readAsBinaryString(file);
+  }
+  private setReaderOnloadHandler() {
+    return () => {
+      let data = this.reader.result;
+      this.workBook = XLSX.read(data, { type: 'binary' });
+      this.workSheetNames = this.workBook.SheetNames;
+      this.workSheets = this.workBook.Sheets;
+
+      for (let i = 0; i < this.workSheetNames.length; i++) {
+        this.currentWorkSheet = this.workSheets[this.workSheetNames[i]];
+        this.currentData = this.currentData.concat(XLSX.utils.sheet_to_json(this.workSheets[this.workSheetNames[i]]));
+      }
+    };
+  }
 }
